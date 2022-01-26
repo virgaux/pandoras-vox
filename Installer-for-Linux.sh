1a09fd3161302883a845c009ae57a8d8e5103331
Nino <nma.tarzona2@gmail.com>
2022-01-26 20:05:36 +0800

    Update Installer-for-Linux.sh

--- Installer-for-Linux.sh
+++ Installer-for-Linux.sh
@@ -3,7 +3,7 @@ echo "       Welcome to Pandoras Vox created by Virgaux."
 echo "  Join the discord server https://discord.gg/ADAdMfug"
 echo "           Close this window to stop the bot"
 echo "----------------------------------------------------------"
-if [-e  node_modules] 
+if [[ -d  ./node_modules ]] 
 then 
 	node index.js
 else 
@@ -12,14 +12,14 @@ else
 fi
 
 
-if [-e  pandoras-vox-linux] 
+if [[ -d  ./pandoras-vox ]]
 then 
-	rmdir -rf node_modules
+	rm -rf node_modules
 	rm -rf index.js
 	rm -rf package.json
 	rm -rf package-lock.json
 	rm -rf README.md
-	rm -rf Installer-for-Windows.bat
+	rm -rf installer-for-Windows.bat
 	read -p "Installation complete. Press any key to continue ..."
 	rm -- "$0"
 else
